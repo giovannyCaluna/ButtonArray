78e569027d1019a9489b3383e991e6b8fdcb79fd
giovannyCaluna <36935405+giovannyCaluna@users.noreply.github.com>
2023-02-11 13:59:46 +0100

    Upload the project.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ButtonArray
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fájlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.setTextColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setBackgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fájlToolStripMenuItem,
+            this.setToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fájlToolStripMenuItem
+            // 
+            this.fájlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fájlToolStripMenuItem.Name = "fájlToolStripMenuItem";
+            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fájlToolStripMenuItem.Text = "&Fájl";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // setToolStripMenuItem
+            // 
+            this.setToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sizeToolStripMenuItem});
+            this.setToolStripMenuItem.Name = "setToolStripMenuItem";
+            this.setToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.setToolStripMenuItem.Text = "&Set";
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.sizeToolStripMenuItem.Text = "&Size";
+            this.sizeToolStripMenuItem.Click += new System.EventHandler(this.sizeToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setTextColorToolStripMenuItem,
+            this.setBackgroundColorToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(190, 70);
+            // 
+            // setTextColorToolStripMenuItem
+            // 
+            this.setTextColorToolStripMenuItem.Name = "setTextColorToolStripMenuItem";
+            this.setTextColorToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.setTextColorToolStripMenuItem.Text = "Set &Text Color";
+            this.setTextColorToolStripMenuItem.Click += new System.EventHandler(this.setTextColorToolStripMenuItem_Click);
+            // 
+            // setBackgroundColorToolStripMenuItem
+            // 
+            this.setBackgroundColorToolStripMenuItem.Name = "setBackgroundColorToolStripMenuItem";
+            this.setBackgroundColorToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.setBackgroundColorToolStripMenuItem.Text = "Set &Background Color";
+            this.setBackgroundColorToolStripMenuItem.Click += new System.EventHandler(this.setBackgroundColorToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 371);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.MainForm_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fájlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem setTextColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setBackgroundColorToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ButtonArray
+{
+    public partial class MainForm : Form
+    {
+        int r = 3 ;    // number of rows
+        int c = 4 ;    // number of columns
+        int xp = 20 ;  // X padding
+        int yp = 20 ;  // Y padding
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetParams dlg = new GetParams();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                MainForm_RemoveButtons(sender, e);
+
+                r = int.Parse(dlg.textBox1.Text);
+                c = int.Parse(dlg.textBox2.Text);
+                xp = int.Parse(dlg.textBox3.Text);
+                yp = int.Parse(dlg.textBox4.Text);
+
+                MainForm_Resize(sender, e);
+
+            }
+        }
+
+        private void MainForm_RemoveButtons(object sender, EventArgs e)
+        {
+            for (int rr = 0; rr < r; ++rr)
+            {
+                for (int cc = 0; cc < c; ++cc)
+                {
+                    string name = "ButtonArray (" + rr.ToString() + ";" + cc.ToString() + ")";
+                    this.Controls.RemoveByKey(name);
+                }
+            }
+            this.Refresh();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            Size s = new Size((this.ClientRectangle.Width - (c + 1) * xp) / c,
+                               (this.ClientRectangle.Height - (r + 1) * yp) / r);
+
+            for (int rr = 0; rr < r; ++rr)
+            {
+                for (int cc = 0; cc < c; ++cc)
+                {
+                    Button b = new Button();
+                    b.Text = "ButtonArray (" + rr.ToString() + ";" + cc.ToString() + ")";
+                    b.Size = s;
+                    b.Location = new Point(xp + cc * (s.Width + xp), yp + rr * (s.Height + yp));
+                    b.Name = b.Text;
+                    b.Click += new EventHandler(ButtonArray_Click);
+                    b.ContextMenuStrip = contextMenuStrip1;
+                    this.Controls.Add(b);
+                }
+            }
+        }
+
+
+        private void ButtonArray_Click(object sender, EventArgs e)
+        {
+            string[] s = sender.ToString().Split(new char [] {'(', ';', ')'}, StringSplitOptions.RemoveEmptyEntries) ;
+            int row = int.Parse(s[1]);
+            int col = int.Parse(s[2]);
+
+            MessageBox.Show("Sor: " + row.ToString() + " oszlop: " + col.ToString());
+        }
+
+        private void setTextColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ContextMenuStrip strip = (sender as ToolStripMenuItem).Owner as ContextMenuStrip;
+            Control pressed = strip.SourceControl as Control;
+
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pressed.ForeColor = dlg.Color;
+            }
+        }
+
+        private void setBackgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ContextMenuStrip strip = (sender as ToolStripMenuItem).Owner as ContextMenuStrip;
+            Control pressed = strip.SourceControl as Control;
+
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pressed.BackColor = dlg.Color;
+            }
+        }
+
+    }
+}
